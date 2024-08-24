938079834ae85039d153d9f8d9784d22e1dfb59f
Meleah Roe <melroe19@gmail.com>
2024-08-24 13:50:51 -0500

    put images in right spot, uploaded some new items to db and created compnents galleryList and Gallery Item

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,15 @@ function App() {
         <p>The gallery goes here!</p>
         <img src="images/goat_small.jpg"/>
         <img src="images/goat_stache.png"/>
+        <img src="images/lanternfest.jpeg/"/> 
+         <img src="images/lil.jpeg"/> 
+          <img src="images/lilchey.jpeg"/>
+          <img src="images/mag.jpeg"/>
+          <img src="images/sam.jpeg"/>
+          <img src="images/flowers.jpeg"/>  
+          <img src="images/mags.jpeg"/>
+          <img src="images/magdoor.jpeg"/>
+
       </div>
     );
 }
